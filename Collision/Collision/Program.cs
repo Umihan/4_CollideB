b6a5bbaba963157ec723e0512b9986e1eeab4bc6
“[UtterJ]” <02huejon@rgtfo-me.it>
2020-05-07 11:17:14 +0200

    Aufgabe A gemacht

--- Collision/Collision/Program.cs
+++ Collision/Collision/Program.cs
@@ -27,9 +27,50 @@ namespace ConsoleApplication1
             public int posx, posy;
             public ConsoleColor farbe;
             // Konstruktor
+
+            Random random_position = new Random();
+            Random random_farbe = new Random();
+
+            /// <summary>
+            /// Hier soll ein neues Objekt initialisiert werden, und zwar muss eine freie Position im Feld zufällig gewählt werden, an dem dieses Objekt seine Startposition hat.Ebenso wird eine Zufallsfarbe (ConsoleColor) für das Objekt gewählt.
+            /// </summary>
             public einer()
             {
+
+                int posx = 0;
+                int posy = 0;
+
+                bool positionfrei = false;
+
+
+                //Objekte werden mit einer zeitverzögerung erstellt
+                System.Threading.Thread.Sleep(20);
+
+
+                //die Eigenschaft "farbe" wird initialisiert
+                farbe = (ConsoleColor)random_farbe.Next(0, 16);
+
+
+                //Finden einer freien Position
+                do
+                {
+                    posx = random_position.Next(1, seite);
+                    posy = random_position.Next(1, seite);
+
+                    if (feld[posx, posy] == 0)
+                    {
+                        positionfrei = true;
+                    }
+                }
+                while (positionfrei == false);
+
+                this.posx = posx;
+                this.posy = posy;
+
+                feld[posx, posy] = 1;
             }
+
+
             //Private Methoden
             void show()
             {
